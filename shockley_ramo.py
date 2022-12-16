805f9168ef2e0440671ca92aba1a73525a68769b
kalie <kalie@berkeley.edu>
2022-12-15 20:12:59 -0800

    anode drift code

--- shockley_ramo.py
+++ shockley_ramo.py
@@ -188,10 +188,10 @@ class charge_transport_model():
         plt.ylabel("Weighting Potential")
         plt.show()
         
-    def drift_particles(self,
+    def drift_particles_cathode(self,
         slice_idx,
         depth_mm,
-        Nt = 6000,
+        Nt = 10000,
         show_plot=False):
         '''
         Drift particles and return Q in au
@@ -299,4 +299,115 @@ class charge_transport_model():
         t90 = np.argwhere(self.Qsignal >= self.Qsignal.max()*.9).flatten()[0]
         rise_time = t90 - t10
         return rise_time
-    
+
+    def drift_particles_anode(self,
+            slice_idx,
+            depth_mm,
+            Nt = 10000,
+            show_plot=False):
+            '''
+            Drift particles and return Q in au
+            
+            Parameters
+            ----------
+            slice_idx: int
+                slice in detector
+            depth_mm: num
+                interaction depth in mm
+            Nt: int
+                time to simulate in ns
+                
+            Returns
+            -------
+            Qsignal_h: ndarray
+                hole signal
+            Qsignal_e: ndarray
+                electron signal
+            Qsignal: ndarray
+                sum of Qh and Qe
+            '''
+            # Vslice = WPslice
+            # holes into wp, electrons out
+            # electron and hole saturation velocities
+            # mobilities taking from literature https://www.sciencedirect.com/science/article/pii/S1738573318303176
+            mu_e = 1350 #cm2/Vs
+            mu_h = 120 #cm2/Vs
+            # Assume parallel plate capacitor, E = V / d
+            # put charge carrier velocity in units of mm/ns 
+            # each time step is 1 ns - just have to add velocity
+            E = 1000 / 1 # 1000 V / 1 cm
+            ve = mu_e * E * 10 ** -9 * 10# mm / ns
+            vh = mu_h * E * 10 ** -9 * 10# mm / ns
+            
+            # grab slice of weighting potential
+            Vslice = self.WP[:,slice_idx]
+            
+            # find initial location idx
+            i0 = np.int(np.floor(depth_mm / self.pixel_size_mm))
+
+            # number of time steps in signal in Nanoseconds
+            Nt = Nt
+
+            # arrays which will store the induced charge signals
+            Qh = np.zeros(Nt, dtype=float)
+            Qe = np.zeros(Nt, dtype=float)
+
+            # starting positions for electrons and holes
+            zh = depth_mm
+            ze = depth_mm
+            ih = i0
+            ie = i0
+
+            # electrons into wp
+            t = 0
+            for t in range(1, Nt):
+                if (ie <= self.N_yelements - 1):
+                    dw = Vslice[ie] - Vslice[ie-1]
+                    Qe[t] = -1.0*dw    
+                elif (ie > self.N_yelements-1):
+                    continue
+                # update new location with hole velocity
+                ze = ze + ve
+                ie = np.int(np.floor(ze / self.pixel_size_mm))
+
+            # holes out of wp
+            t = 0
+            for t in range(1, Nt):
+                if (ih >= 0):
+                    dw = Vslice[ih] - Vslice[ih+1]
+                    Qh[t] = 1.0*dw
+                elif (ih < 0):
+                    continue
+                # update new location with electron velocity
+                zh = zh - vh
+                ih = np.int(np.floor(zh / self.pixel_size_mm))
+
+            # take cumulative sums
+            self.Qsignal_h = np.cumsum(Qh)
+            self.Qsignal_e = np.cumsum(Qe)
+            self.Qsignal = np.cumsum(Qe + Qh)
+
+            
+            if show_plot:
+            
+                # plot results
+                # plot
+                plt.figure()
+                plt.plot(self.Qsignal_e, 'm', linewidth=1.5,label='Qe')
+                plt.plot(self.Qsignal_h, 'c', linewidth=1.5,label='Qh')
+                plt.plot(self.Qsignal, 'k', linewidth=2,label='Qsignal')
+                plt.grid("on")
+                #plt.ylim(0,1)
+                plt.xlim(0, Nt)
+                plt.tick_params(labelbottom="off")
+                plt.xlabel("Time (ns)")
+                plt.ylabel("Charge (au)")
+                plt.title('Signal for interaction at '+str(depth_mm)+' mm depth')
+                plt.legend()
+                plt.show()
+                
+            # find rise time 
+            t10 = np.argwhere(self.Qsignal >= self.Qsignal.max()*.1).flatten()[0]
+            t90 = np.argwhere(self.Qsignal >= self.Qsignal.max()*.9).flatten()[0]
+            rise_time = t90 - t10
+            return rise_time
